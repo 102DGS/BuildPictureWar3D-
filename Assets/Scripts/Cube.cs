28659ab85f2985db5caab8d917f20fd57777a43c
colodezzzz <71928986+102DGS@users.noreply.github.com>
2020-10-09 17:54:19 +0300

    Adding falling for cubes

---
+++ Assets/Scripts/Cube.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    private Vector3 startPosition;
+    private Quaternion startRotation;
+
+    private void Awake()
+    {
+        startPosition = transform.position;
+        startRotation = transform.rotation;
+    }
+
+    void Update()
+    {
+        if (transform.position.y <= -15f)
+        {
+            transform.position = startPosition + new Vector3(0f, 0.5f, 0f);
+            transform.rotation = startRotation;
+        }
+    }
+}
