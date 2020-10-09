a67e88649ab70c826cf86d2565b513a9db1ed80f
colodezzzz <71928986+102DGS@users.noreply.github.com>
2020-10-09 19:08:20 +0300

    Added place picture

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -7,6 +7,18 @@ public class Cube : MonoBehaviour
     private Vector3 startPosition;
     private Quaternion startRotation;
 
+    public Vector3 CurrentPosition 
+    {
+        get { return transform.position; }
+        set { transform.position = value; }
+    }
+
+    public Quaternion CurrentRotation
+    {
+        get { return transform.rotation; }
+        set { transform.rotation = value; }
+    }
+
     private void Awake()
     {
         startPosition = transform.position;
