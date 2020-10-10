55e2c5071d4ef938748a4ddfa6210fb5049d7953
JayKimyra <KiraKimyra@gmail.com>
2020-10-10 19:39:21 +0300

    StaticClass

--- Assets/Scripts/DebugRayForward.cs
+++ Assets/Scripts/DebugRayForward.cs
@@ -10,9 +10,14 @@ public class DebugRayForward : MonoBehaviour
         
     }
 
-    void Update()
+    void Awake()
     {
         Debug.DrawRay(transform.position,transform.forward*100,Color.yellow);
+        Color[] pixelsColor = StaticClass.imageByteArray(Application.dataPath + "/1.png");
+        foreach (var pixel in pixelsColor)
+        {
+           Debug.Log(pixel);
+        } 
 
     }
 }
