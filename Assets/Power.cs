2fa03e61569b5dc8e6dbe4c79b9d2070b1e63dd8
JayKimyra <KiraKimyra@gmail.com>
2020-10-11 15:30:52 +0300

    Menu scene

---
+++ Assets/Power.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Power : MonoBehaviour
+{
+    public GameObject Cubes;
+    Transform target;
+    void Start()
+    {
+        var elements = Cubes.GetComponentsInChildren<Transform>();
+        target = elements[Random.Range(0, elements.Length)];
+        
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) Shoot();
+    }
+    void Shoot()
+    {
+        Debug.Log("Shoot");
+        Vector3 vectorToTarget = transform.position - target.position;
+        GetComponent<Rigidbody>().AddForce(vectorToTarget * Random.Range(10,100),ForceMode.Impulse);
+    }
+}
