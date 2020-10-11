19d1b12bb31f14c6f4d5a474665b4f88658ada2e
colodezzzz <71928986+102DGS@users.noreply.github.com>
2020-10-11 16:27:42 +0300

    Succesfull done test with text in script

---
+++ Assets/Scripts/ChangeText.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeText : MonoBehaviour
+{
+    int startFontSize;
+    Vector3 startScale;
+
+    private void Awake()
+    {
+        startScale = GetComponent<Button>().transform.localScale;
+    }
+
+    void OnMouseOver()
+    {
+        GetComponent<Button>().transform.localScale = startScale + new Vector3(0.5f, 0.5f, 0f);
+    }
+
+    void OnMouseExit()
+    {
+        GetComponent<Button>().transform.localScale = startScale;
+    }
+}
