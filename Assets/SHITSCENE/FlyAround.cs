70022773b0d5920c67040e3ebcc5ece1ced1b1ab
JayKimyra <KiraKimyra@gmail.com>
2020-10-11 16:27:12 +0300

    Create MainMenu scene

--- Assets/SHITSCENE/FlyAround.cs
+++ Assets/SHITSCENE/FlyAround.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class FlyAround : MonoBehaviour
 {
+    Rigidbody rb;
+    public float minSpeed;
+    public float maxSpeed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        rb.angularVelocity = new Vector3(0, Random.Range(minSpeed, maxSpeed), 0);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
