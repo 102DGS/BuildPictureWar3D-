16bf2014e7bad8a550b37ee106a003941a96f7ec
colodezzzz <71928986+102DGS@users.noreply.github.com>
2020-10-12 20:42:22 +0300

    Made functional for menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        /*SceneManager.LoadScene(1);*/
+        Debug.Log("No start yet");
+    }
+
+    public void Settings()
+    {
+        Debug.Log("No settings yet");
+    }
+
+    public void QuitGame()
+    {
+        /*Application.Quit();*/
+        Debug.Log("No quit yet");
+    }
+}
