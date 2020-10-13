ce84f8eee79fa18a059c2bf4b69eff9a96830c2e
colodezzzz <71928986+102DGS@users.noreply.github.com>
2020-10-13 16:48:42 +0300

    Create timer in Sanya'sZone

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    private float leftTime = 20;
+    public Text timerText;
+
+    private void Awake()
+    {
+        timerText.text = currentTime(leftTime);
+    }
+
+    void Update()
+    {
+        leftTime -= Time.deltaTime;
+        if (leftTime > 0)
+        {
+            timerText.text = currentTime(leftTime);
+        }
+        else
+        {
+            timerText.text = "Time left";
+        }
+    }
+
+    private string currentTime(float leftTime)
+    {
+        if ((int)Mathf.Round(leftTime) % 60 < 10)
+        {
+            return ($"Timer: {(int)Mathf.Round(leftTime) / 60}:0{(int)Mathf.Round(leftTime) % 60}");
+        }
+        else
+        {
+            return ($"Timer: {(int)Mathf.Round(leftTime) / 60}:{(int)Mathf.Round(leftTime) % 60}");
+        }
+    }
+}
