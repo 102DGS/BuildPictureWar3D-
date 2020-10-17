cfab1b6575f4ddd6e4a6df567414d6a91fc167b1
colodezzzz <71928986+102DGS@users.noreply.github.com>
2020-10-17 18:15:55 +0300

    Useless fixes

--- Assets/Scripts/ChangeText.cs
+++ Assets/Scripts/ChangeText.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class ss : EventTrigger
+public class ChangeText : EventTrigger
 {
     private int startFontSize;
 
