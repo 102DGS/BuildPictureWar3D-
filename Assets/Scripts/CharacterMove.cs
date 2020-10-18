3b942da4209ea125d6bf91d84a673f63b82b8689
colodezzzz <71928986+102DGS@users.noreply.github.com>
2020-10-18 22:28:01 +0300

    Create new character

---
+++ Assets/Scripts/CharacterMove.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMove : MonoBehaviour
+{
+    private float speed = 5f;
+    private float jumpForce = 5f;
+    private Rigidbody _rigidbody;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
+        {
+            Run();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+
+    private void Run()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        Vector3 direction = transform.right * x + transform.forward * z;
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + direction, speed * Time.deltaTime);
+    }
+
+    private void Jump()
+    {
+        _rigidbody.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+}
