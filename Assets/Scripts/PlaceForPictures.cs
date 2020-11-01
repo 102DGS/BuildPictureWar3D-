2598dadf15214db79be2c4864076c25746d59d23
JayKimyra <KiraKimyra@gmail.com>
2020-11-01 21:48:07 +0300

    дщщд

--- Assets/Scripts/PlaceForPictures.cs
+++ Assets/Scripts/PlaceForPictures.cs
@@ -22,8 +22,8 @@ public class PlaceForPictures : MonoBehaviour
 
     private void Awake()
     {
-        //_picture = new Picture(1+Random.Range(0, Picture.currentPictures));
-        _picture = new Picture(15);
+        _picture = new Picture(1+Random.Range(0, Picture.currentPictures));
+        //_picture = new Picture(15);
         colors = _picture.colors;
         rows = _picture.height;
         columns = _picture.width;
